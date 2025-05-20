4cfd836e2d4e7ff308e92413510831804e4b4f03
Abraham Justin <162578329+abrahamjust@users.noreply.github.com>
2025-05-20 14:45:06 +0000

    created js files for each page

---
+++ src/aboutPage.js
@@ -0,0 +1,36 @@
+export { aboutFunction }
+
+const aboutH2Content = "About Us";
+const aboutParaContent = `<em>The Spork</em> is where comfort food meets creative flair. 
+                    Since 2022, we've served a bold fusion of global street flavors and home-style classics — all crafted with fresh, locally sourced ingredients. 
+                    Quirky, casual, and always delicious, our space is perfect for quick bites, chill brunches, or spontaneous food adventures.`;
+const aboutPara2Content = `Come hungry, leave happy — that's the Spork way.`;
+
+const contactH2Content = "Contact Us";
+const contactParaContent = `Address: 123 Flavor Lane, Chungus city, ZZ 45678 <br>
+                    Phone no: (555) 123-4567 <br>
+                    email: hello@thesporkrestaurant.com <br>
+                    Timings: Mon-Fri: 9 AM - 10 PM | Sat-Sun: 8 AM - 11 PM`;
+
+function aboutFunction() {
+    const aboutDiv = document.createElement("div");
+    aboutDiv.className = "about";
+
+    const aboutH2 = document.createElement("h2");
+    aboutH2.textContent = aboutH2Content;
+    const aboutPara = document.createElement("p");
+    aboutPara.textContent = aboutParaContent;
+    const aboutPara2 = document.createElement("p");
+    aboutPara2.textContent = aboutPara2Content;
+
+    aboutDiv.append(aboutH2, aboutPara, aboutPara2);
+
+    const contactH2 = document.createElement("h2");
+    contactH2.textContent = contactH2Content;
+    const contactPara = document.createElement("p");
+    contactPara.textContent = contactParaContent;
+
+    aboutDiv.append(contactH2, contactPara);
+
+    document.getElementById("content").appendChild(aboutDiv);
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import "./styles.css";
 import backgroundImg from "./assets/restaurant_background.jpg";
-//import { homeFunction } from "./homePage.js";
-//import { menuFunction } from "./menuPage.js";
+import { homeFunction } from "./homePage.js";
+import { menuFunction } from "./menuPage.js";
+import { aboutFunction } from "./aboutPage.js";
 
-console.log("hello, world");
-
+// background image
 const image = document.createElement("img");
 image.src = backgroundImg;
 image.alt = "Background Image";
 image.id = "background";
 document.body.appendChild(image);
 
+
+
+
 //menuFunction();
 //homeFunction();
+//aboutFunction();
